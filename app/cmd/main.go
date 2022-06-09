cb50bc1156de789770da86557cea726cca9ee907
Danut Avadanei <danut.avadanei@s.utm.ro>
2022-06-09 19:59:36 +0300

    Cleanup

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -135,67 +135,6 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		_, _ = w.Write(bytes)
 	}).Methods("GET").Name("listSessions")
-	/*
-		m.HandleFunc("/s3/buckets/{bucket}", func(w http.ResponseWriter, r *http.Request) {
-			vars := mux.Vars(r)
-			bucket := vars["bucket"]
-
-			bytes, err := awsClient.ListBucketObjects(r.Context(), &bucket)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			_, _ = w.Write(bytes)
-		}).Methods("GET").Name("listBucketObjects")
-
-		m.HandleFunc("/s3/buckets/{bucket}", func(w http.ResponseWriter, r *http.Request) {
-			vars := mux.Vars(r)
-			bucket := vars["bucket"]
-
-			err := r.ParseMultipartForm(32 << 20) // maxMemory 32MB
-
-			if err != nil {
-				log.Printf("error parsing request: %v", err)
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			f, h, err := r.FormFile("file")
-
-			if err != nil {
-				log.Printf("error parsing request: %v", err)
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			bytes, err := awsClient.UploadFileToBucket(r.Context(), &bucket, &h.Filename, f)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			_, _ = w.Write(bytes)
-		}).Methods("POST", "PUT").Name("putBucketObject")
-
-		m.HandleFunc("/s3/buckets/{bucket}/{key}", func(w http.ResponseWriter, r *http.Request) {
-			vars := mux.Vars(r)
-			bucket, key := vars["bucket"], vars["key"]
-
-			bytesWritten, err := awsClient.SinkFileToWriter(r.Context(), &bucket, &key, w)
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			log.Printf("Download of \"%s\" complete. Wrote %s bytes", key, strconv.FormatInt(bytesWritten, 10))
-		}).Methods("GET").Name("getBucketObject")
-	*/
 
 	srvShutdown := make(chan bool)
 	srv := server.StartHttpServer(cfg.HTTPServerConfig, m, srvShutdown)
