442adf6815450dcfb675a8f4c30ef27f4e0903b4
Danut Avadanei <danut.avadanei@s.utm.ro>
2022-06-10 15:37:30 +0300

    Bind data to profesor view

--- web/src/views/ProfessorIndexView.vue
+++ web/src/views/ProfessorIndexView.vue
@@ -14,7 +14,7 @@
               <thead class="bg-gray-50">
                 <tr>
                   <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Session Id</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">User name</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">User Email</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Lab Type</th>
                   <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
                     <span class="sr-only">Monitor</span>
@@ -24,8 +24,8 @@
               <tbody class="divide-y divide-gray-200 bg-white">
                 <tr v-for="session in sessions" :key="session.id">
                   <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ session.id }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ session.user_name }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ session.lab_type }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ session.user.email }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ session.lab.type }}</td>
                   <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                     <router-link :to="{ name: 'monitor', params: { session: session.id }}" class="text-indigo-600 hover:text-indigo-900">
                       Monitor
@@ -47,18 +47,12 @@ import { reactive, inject, onMounted } from 'vue'
 import { RouterLink } from 'vue-router'
 
 const axios = inject('axios')
-const sessions = reactive([
-  { id: 1, user_name: 'dan', lab_type: 'windows' },
-  { id: 2, user_name: 'john', lab_type: 'windows' },
-  { id: 3, user_name: 'jane', lab_type: 'kali' },
-  { id: 4, user_name: 'ben', lab_type: 'kali' },
-])
+const sessions = reactive([])
 const apiBaseUrl = inject('apiBaseUrl')
-const apiEndpoint = `${apiBaseUrl}/s3/buckets`;
+const apiEndpoint = `${apiBaseUrl}/v1/sessions`;
 
-// onMounted(async () => {
-//   await axios.get(apiEndpoint)
-//     .then(response => buckets.push(...response.data.Buckets))
-// })
-
+onMounted(async () => {
+  await axios.get(apiEndpoint)
+    .then(response => sessions.push(...response.data))
+})
 </script>
